0e0fd8042dcf549225967e2db7ae3575bb7d6e4f
dybik08 <dybik08@gmail.com>
2018-11-05 18:05:13 +0100

    Show result for player - basic view

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 window.onload = () => {
 
-    let word ;
-    let counter ;
+    const guessedLettersArray = [];
+    let letterChosenByPlayer;
+    let triesCounter = 0;
+    let space = 0;
 
-    const characters = [
+
+    const charactersArray = [
         'a','ą','b','c','ć','d','e','ę','f','g','h','i','j','k','l','ł','m','n','ń','o','ó','p','r','s','ś','t','q','u','w','x','y','z','ź','ż'
     ];
 
@@ -11,34 +14,37 @@ window.onload = () => {
         'truskawka', 'melon', 'cytryna', 'śliwka', 'pomarańcza'
     ];
 
-    selectWord = () => {
-      return word = fruits[Math.floor(Math.random()*fruits.length)];
+    selectWordToGuess = () => {
+      return fruits[Math.floor(Math.random()*fruits.length)];
     };
 
-    createLetterButtons = () => {
-        const letters = document.getElementById('letters');
-        const letterList = document.createElement('ul');
+    let wordSelectedForCurrentGame = selectWordToGuess();
+    wordSelectedForCurrentGame = wordSelectedForCurrentGame.replace(/\s/g, "-");
+    console.log(wordSelectedForCurrentGame);
 
-        for(let i = 0; i < characters.length; i++){
-            let list = document.createElement('li');
-            letterList.id = 'characters';
-            list.id = 'letter';
-            list.innerHTML = characters[i];
-            letters.appendChild(letterList);
-            letterList.appendChild(list);
-            onLetterClick(list);
+    createLettersButtonsList = () => {
+        const lettersButtonsDiv = document.getElementById('lettersButtonsDiv');
+        const lettersButtonsUl = document.createElement('ul');
+
+        for(let i = 0; i < charactersArray.length; i++){
+            let letterButtonLi = document.createElement('li');
+            lettersButtonsUl.id = 'lettersButtonsUl';
+            letterButtonLi.id = 'letter';
+            letterButtonLi.innerHTML = charactersArray[i];
+            lettersButtonsDiv.appendChild(lettersButtonsUl);
+            lettersButtonsUl.appendChild(letterButtonLi);
+            onLetterClick(letterButtonLi);
         }
     };
 
 
-    onLetterClick = (list) => {
-        list.onclick = () => {
-            let geuss = (list.innerHTML);
-            list.setAttribute("class", "active");
-            let word = selectWord();
-            for (let i = 0; i < word.length; i++) {
-                if (word[i] === geuss) {
-                    counter += 1;
+    onLetterClick = (letterButtonLi) => {
+        letterButtonLi.onclick = () => {
+            letterChosenByPlayer = (letterButtonLi.innerHTML);
+            letterButtonLi.setAttribute("class", "active");
+            for (let i = 0; i < wordSelectedForCurrentGame.length; i++) {
+                if (wordSelectedForCurrentGame[i] === letterChosenByPlayer) {
+                    triesCounter += 1;
                 }
             }
         };
@@ -46,6 +52,26 @@ window.onload = () => {
 
     };
 
-    createLetterButtons();
+    showResultForPlayer = () => {
+        const wordToGuessDiv = document.getElementById('selectedWordDisplayerDiv');
+        const wordToGuessUl = document.createElement('ul');
+        for (let i = 0; i < wordSelectedForCurrentGame.length; i++) {
+            wordToGuessUl.setAttribute('id', 'selectedWordDisplayerUl');
+            letterChosenByPlayer = document.createElement('li');
+            letterChosenByPlayer.setAttribute('class', 'selectedWordDisplayerLi');
+            if (wordSelectedForCurrentGame[i] === "-") {
+                letterChosenByPlayer.innerHTML = "-";
+                space = 1;
+            } else {
+                letterChosenByPlayer.innerHTML = "_";
+            }
+            guessedLettersArray.push(letterChosenByPlayer);
+            wordToGuessDiv.appendChild(wordToGuessUl);
+            wordToGuessUl.appendChild(letterChosenByPlayer);
+        }
+    };
+
+    createLettersButtonsList();
+    showResultForPlayer();
 };
 
